4681a094eba5adb6ddf211e2fdb60d088e26a326
nicoobonora <nicobonoraaa@gmail.com>
2026-03-10 18:25:00 +0100

    fix: ui navbar fix

--- app/(club-site)/admin/(protected)/layout.tsx
+++ app/(club-site)/admin/(protected)/layout.tsx
@@ -49,7 +49,7 @@ export default async function ClubAdminLayout({
   return (
     <div className="flex min-h-screen">
       <AdminSidebar clubName={club.name} basePath={basePath} />
-      <main id="main-content" className="min-w-0 flex-1 bg-background">
+      <main id="main-content" className="min-w-0 flex-1 bg-background pt-[60px] lg:pt-0">
         <div className="container-sportbook py-8">
           {children}
         </div>
--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -156,8 +156,6 @@ export function AdminSidebar({ clubName, basePath = "" }: { clubName: string; ba
         </Button>
       </div>
 
-      {/* Spacer per la barra mobile fissa */}
-      <div className="h-[60px] lg:hidden" />
     </>
   )
 }
